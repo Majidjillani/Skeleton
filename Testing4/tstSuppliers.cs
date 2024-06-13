4200d32506a1b83917ac789f1e6f818d50c04e0d
majid <majid@MAJID>
2024-06-13 20:19:00 +0100

    Majid

---
+++ Testing4/tstSuppliers.cs
@@ -0,0 +1,20 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing4
+{
+    [TestClass]
+    public class tstSuppliers
+    {
+        /******************INSTANCE OF THE CLASS TEST******************/
+        [TestMethod]
+        public void InstanceOK()
+        {
+            // Creating an instance of the class I want to create
+            clsSuppliers ASupplier = new clsSuppliers();
+            // Testing to see that it actually exists
+            Assert.IsNotNull(ASupplier);
+        }
+    }
+}
